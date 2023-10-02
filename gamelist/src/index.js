aad8b2bb0d4e6554cc5aee50f4a3e349509048cb
Rishabh Dev <devrishabh28@gmail.com>
2023-10-02 19:30:59 +0530

    dynamic componenets using map and different methods to pass props

--- gamelist/src/index.js
+++ gamelist/src/index.js
@@ -3,35 +3,40 @@ import ReactDOM from "react-dom/client"
 
 import './/css/index.css';
 
-
-const GameList = () => {
-    
-    const firstGame = {
+const games = [
+    {
+        id: 1,
         creator:'FromSoftware',
         title:'Elden Ring',
         img:'https://image.api.playstation.com/vulcan/ap/rnd/202108/0410/2odx6gpsgR6qQ16YZ7YkEt2B.png'
-    }
-
-    const secondGame = {
+    },
+    {
+        id: 2,
         creator:'FromSoftware',
         title:'Dark Souls III',
         img:'https://image.api.playstation.com/cdn/EP0700/CUSA03365_00/OFMeAw2KhrdaEZAjW1f3tCIXbogkLpTC.png'
     }
+]
 
+/* const GameList = () => {
     return (
         <section className='gamelist'>
-            <Game creator={firstGame.creator} title={firstGame.title} img={firstGame.img} >
-                <p>Elden Ring is a 2022 action role-playing game developed by FromSoftware. It was directed by Hidetaka Miyazaki with worldbuilding provided by fantasy writer George R. R. Martin.</p>
-                <button>Purchase</button>
-            </Game>
-            <Game creator={secondGame.creator} title={secondGame.title} img={secondGame.img} />
+            {games.map((game) => <Game creator={game.creator} title={game.title} img={game.img} key={game.id} />)}
+        </section>
+    );
+} */
+
+const GameList = () => {
+    return (
+        <section className='gamelist'>
+            {games.map((game) => <Game game={game} key={game.id} />)}
         </section>
     );
 }
 
 // const Game = (props) => {
 //     console.log(props);
-//     const {creator, title, img} = props;
+//     const {creator, title, img} = props.game;
 //     return (
 //         <article className='game'>
 //             <img src={img} alt={title} />
@@ -41,7 +46,7 @@ const GameList = () => {
 //     );
 // }
 
-const Game = ({creator, title, img, children}) => {
+const Game = ({game: {creator, title, img, children}}) => {
     return (
         <article className='game'>
             <img src={img} alt={title} />
